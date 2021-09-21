db4f3fbb3694d88c85e199592a991f95a3a44043
nebula <yzh.nebula@foxmail.com>
2021-09-21 17:32:36 +0800

    modify

--- springboot-practice/springboot-01-quickstart/src/main/java/com/spring/springboot01quickstart/controller/HelloController.java
+++ springboot-practice/springboot-01-quickstart/src/main/java/com/spring/springboot01quickstart/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.spring.springboot01quickstart.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @RequestMapping("/hello")
-    public String hello() {
-        return "quick start!";
+    @RequestMapping("/home.html")
+    public void home() {
     }
 }
